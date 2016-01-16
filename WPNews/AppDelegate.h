3926fc31b335e6a6697fbc7ac4e2bc2fcf9a827a
Lyndsey Ferguson <lyndsey.ferguson@gmail.com>
2016-01-16 15:20:09 -0500

    Initial Commit

---
+++ WPNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WPNews
+//
+//  Created by Lyndsey on 1/16/16.
+//  Copyright © 2016 Lyndsey Ferguson Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WPNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WPNews
+//
+//  Created by Lyndsey on 1/16/16.
+//  Copyright © 2016 Lyndsey Ferguson Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
